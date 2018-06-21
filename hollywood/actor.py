412118e20587231a1bc07485d66342672657e977
Joao Grilo <joao.grilo@gmail.com>
2018-06-21 02:22:37 +0200

    Add some documentation to make the intent clear.

--- hollywood/actor.py
+++ hollywood/actor.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+"""
+    Actor base class and backends.
+
+    An actor:
+        - Is an independent unit of computation.
+        - May have one or more addresses.
+        - Has an inbox (the code below implements this with a Queue).
+        - Processes messages from the inbox sequentially.
+        - May create other actors.
+        - May communicate with other actors via their address.
+
+    An actor is no actor. Actors are useful when they come in systems.
+    Systems are a set of actors which provide some actual functionality
+    and enable all the actors to work with eachother.
+
+    In pure implementions, the inbox of an actor would be an actor in
+    itself.
+"""
+
 import time
 import Queue
 import threading
@@ -8,7 +27,22 @@ import uuid
 
 
 class Base(object):
+    """Abstract-y class that implements a synchronous actor.
 
+    A synchronous actor is completely unusable, do not subclass this directly.
+    Instead, prefer one of the backends (Threaded).
+
+    The start/stop method control whether the event loop is running or not.
+
+    For every iteration of the event loop, one message is extracted from the
+    inbox and passed as-is to the receive(*message) method.
+
+    The method 'tell' is "fire-and-forget": will cause the actor to do some work
+    but always returns None.
+
+    The method 'ask' is very similar to tell, but returns a future with the
+    result of the computation.
+    """
 
     def __init__(self):
         self.inbox = Queue.Queue()
@@ -45,17 +79,32 @@ class Base(object):
 
 
 class Threaded(Base):
+    """
+        Spawn the event loop in a new thread.
+
+        If 'ask'ed, computes the result in a separate thread
+        and returns a future.
+    """
 
 
     def start(self):
         threading.Thread(target=self._loop).start()
 
     def ask(self, *args, **kwargs):
+        """Return a future with the result.
+
+        This is specific implementation skips the actor's inbox and directly
+        runs the computation in a separate thread.
+
+        In regular circumstances, the 'ask' would be put in the inbox and
+        processed with no higher priority, but I didn't find an elegant way
+        to implement that.
+        """
         queue = Queue.Queue()
-        threading.Thread(target=self.future, args=(queue, args), kwargs=kwargs).start()
+        threading.Thread(target=self.__future, args=(queue, args), kwargs=kwargs).start()
         return queue
 
-    def future(self, queue, args, **kwargs):
+    def __future(self, queue, args, **kwargs):
         """
             Wraps the 'receive' method by capturing its output and returning
             it in the form of a Queue.Queue.
